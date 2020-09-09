97b34c33fa4eed16e2de0c97b2c1421dc465c18b
Siddharth Belbase <siddharthbelbase@gmail.com>
2020-09-09 13:42:17 +0545

    📝 Beautified Stuffs

--- blocker.py
+++ blocker.py
@@ -11,48 +11,55 @@ which you aren't following, but those accounts who you haven't shared that magic
 script is super useful when you don't want to get followed by useless bots, non-active users & don't know maybe 
 some other personal issues. This script is tested, trusted and checked 127 times, so every part works just fine.
 
-!!! Disclaimer: But though, RUN at your own risk (Just kidding, I know you want this badly.)
+!!! Disclaimer: But though, RUN this script at your own risk (Just kidding, I know you want this.)
 
 Steps:
 
 Install tweepy module using pip.
-Locate keys.py and fill the details like author, consumer_key, consumer_secret, access tokens, token secrets
-Visit https://apps.twitter.com to get your credentials i.e. consumer keys and stuffs.
+To run this, you need a twitter developer account so, you'll need one.
+Visit https://apps.twitter.com/ to get your credentials as given below.
+Fill all the details like AUTHOR, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, TOKEN_SECRET.
 
-In short to run this, you need a developer account & need to initialize an application. Then you're good to go.
+NOTE: Alternately you could use gist:
+ 
+ https://gist.github.com/sidbelbase/4b5b484d39f0f6de29d76d46be5db289
+
 '''
 
 followers = []
 followings = []
 
+
 def sayno(anything_thats_countable):
-	if int(len(anything_thats_countable)) == 0:
-		return 'None'
-	else:
-		return len(anything_thats_countable)
+    if int(len(anything_thats_countable)) == 0:
+        return 'None'
+    else:
+        return len(anything_thats_countable)
 
 
 def initalize_the_blocker(the_thing_to_be_counted):
-	if int(len(the_thing_to_be_counted)) > 0:
-		print('\n')
-		print('Now, initializing block operation...')
-		sleep(1)
-		print('Blocking non-mutual followers...')
+    if int(len(the_thing_to_be_counted)) > 0:
+        print('\n')
+        print('Now, initializing block operation...')
+        sleep(1)
+        print('Blocking non-mutual followers...')
 
-		for i,uncommoner in enumerate(uncommons):
-			username = api.get_user(uncommoner)
-			api.create_block(uncommoner)
-			api.destroy_block(uncommoner)
-			print(f'{i+1}. {username.screen_name} unfollowed you.')
-			sleep(5)
-		print(f'\nYou\'re safe now. We removed {len(uncommons)} non-mutuals from your account.')
-	else:
-		print('\nSeems like you didn\'t have any non-mutuals, so we didn\'t remove anyone.')
+        for i, uncommoner in enumerate(uncommons):
+            username = api.get_user(uncommoner)
+            api.create_block(uncommoner)
+            api.destroy_block(uncommoner)
+            print(f'{i+1}. {username.screen_name} unfollowed you.')
+            sleep(5)
+        print(
+            f'\nYou\'re safe now. We removed {len(uncommons)} non-mutuals from your account.')
+    else:
+        print('\nSeems like you didn\'t have any non-mutuals, so we didn\'t remove anyone.')
 
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+api = tweepy.API(auth, wait_on_rate_limit=True,
+                 wait_on_rate_limit_notify=True, compression=True)
 
 sleep(1)
 print('Note: This script blocks those accounts that you don\'t follow.')
@@ -63,16 +70,17 @@ sleep(1)
 print('\n')
 
 for friend in tweepy.Cursor(api.friends_ids, screen_name=AUTHOR).pages():
-	followings.extend(friend)
+    followings.extend(friend)
 
 print(f'\tFollowings: {sayno(followings)}')
 
 for follower in tweepy.Cursor(api.followers_ids, screen_name=author).pages():
-	followers.extend(follower)
+    followers.extend(follower)
 
 print(f'\tFollowers: {sayno(followers)}')
 
-commons = [friend for follower in followers for friend in followings if friend == follower ]
+commons = [
+    friend for follower in followers for friend in followings if friend == follower]
 
 print(f'\tMutual Followers: {sayno(commons)}')
 
